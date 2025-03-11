e2c5a52e7970005a925510acb88656476f0f3ed2
abhinav5806 <121560977+abhinav5806@users.noreply.github.com>
2025-03-11 17:48:00 +0000

    datatype conversion

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+let score = 1234
+let stringscore = String(score)
+console.log(typeof stringscore);
+console.log(stringscore);
+
+let anystring = "abhinav123"
+let numstring = Number(anystring)
+console.log(typeof numstring);
+console.log(numstring);
+
